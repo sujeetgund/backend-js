5d50f2075021afd83e3c889a5c54dcd92f905195
Sujeet Gund <sujeetgund@gmail.com>
2024-04-26 15:07:52 +0530

    add custom error and response utils classes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is listening on PORT: ${process.env.PORT}`);
+  });
+});
 
 /*
 import express from "express";
